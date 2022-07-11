1ab440497abda64f4330487ae38d53b27a680131
jgjefersonluis <jgjefersonluis@gmail.com>
2022-07-11 16:38:30 -0300

    property - gets e setters

--- models/cliente.py
+++ models/cliente.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from utils.helper import str_para_date
+from utils.helper import str_para_date, date_para_str
 
 
 class Cliente:
@@ -14,4 +14,56 @@ class Cliente:
         self.__data_nascimento: date = str_para_date(data_nascimento)
         self.__data_cadastro: date = date.today()
         Cliente.contador += 1
+
+    @property
+    def codigo(self: object) -> int:
+        return self.__codigo
+
+    @property
+    def nome(self: object) -> str:
+        return self.__nome
+
+    @ property
+    def email(self: object) -> str:
+        return self.__email
+
+    @property
+    def cpf(self: object) -> str:
+        return self.__cpf
+
+    @property
+    def data_nascimento(self:object) -> str:
+        return date_para_str(self.__data_nascimento)
+
+    @property
+    def data_cadastro(self: object) -> str:
+        return date_para_str(self.__data_cadastro)
+
+    def __str__(self: object) -> str:
+        return f'Codigo: {self.codigo} \nNome: {self.nome}\nData de Nascimento: {self.data_nascimento}\nCadastro: {self.data_cadastro}'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
